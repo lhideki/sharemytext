04d33713c222925d33e6ddd027fa214ed5f03305
Hideki INOUE <hideki@inoue-kob.ocm>
2018-12-01 12:42:14 +0000

    First commit

--- src/mytext.py
+++ src/mytext.py
@@ -1,9 +1,13 @@
 import json
 import datetime
 import logging
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+dynamodb = boto3.resource('dynamodb')
+texts_table = dynamodb.Table('sharemytext.Texts')
 
 def create_response(status_code = 200, body = None):
     res = {
@@ -15,14 +19,27 @@ def create_response(status_code = 200, body = None):
 
 def get(event, context):
     id = event['pathParameters']['id']
-    text = id
-    
-    data = {
-        'output': text,
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
+    res = texts_table.get_item(
+        Key = {
+            "id": id
+        }
+    )
 
-    return create_response(body = json.dumps(data))
+    if 'Item' in res:
+        text = res['Item']
+        data = {
+            'isExists': True,
+            'text': text,
+            'timestamp': datetime.datetime.utcnow().isoformat()
+        }
+    else:
+        data = {
+            'isExists': False,
+            'text': None,
+            'timestamp': datetime.datetime.utcnow().isoformat()
+        }
+
+    return create_response(body = data)
 
 def post(event, context):
     id = event['pathParameters']['id']
@@ -31,9 +48,19 @@ def post(event, context):
     data = {
         'id': id,
         'text': text,
-        'lastUpdatedDate': now
+        'lastUpdatedDate': now.isoformat()
     }
     
     logger.info(data)
+    res = texts_table.put_item(
+        Item = data
+    )
+    status_code = res['ResponseMetadata']['HTTPStatusCode']
     
-    return create_response()
+    if status_code > 200:
+        return create_response(status_code = status_code,
+            body = {
+                "status": "Creation error"
+            })
+    else:
+        return create_response(body = {"status": "OK"})
